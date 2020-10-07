ef52bf5ae1fd2217b9fdb990185f772ce083a493
BidinhoSousa <davidhenriquesdesousa@gmail.com>
2020-10-07 20:06:06 +0100

    added module 5 assignment and some changes to js testing

--- JavaScript_testing/js/Script.js
+++ JavaScript_testing/js/Script.js
@@ -1,17 +1,20 @@
-// Function constructors
-function Circle (radius) {
-    this.radius = radius;
-  }
-  
-  Circle.prototype.getArea = 
-    function () {
-      return Math.PI * Math.pow(this.radius, 2);
-    };
-  
-  
-  var myCircle = new Circle(10);
-  console.log(myCircle.getArea());
-  
-  var myOtherCircle = new Circle(20);
-  console.log(myOtherCircle);
-  
+
+
+function sayHello (event){
+  document.getElementById("buttonSubmite").textContent="Submitted!"
+  var name=document.getElementById("inName").value;
+  console.log(event);
+  console.log(name);
+  // var message= "<h1>"+name+" ,queres fazer cocó? </h1>";
+  // console.log(message);
+  // document.getElementById("Resposta").innerHTML=message;
+  document.getElementById("Resposta").innerHTML="<h1>"+name+" ,queres fazer cocó? </h1>";
+}
+
+document.querySelector("#buttonSubmite").addEventListener("click", sayHello);
+document.querySelector("#inName").addEventListener("keypress", function (e) {
+  if(e.key=="Enter")
+  sayHello();
+});
+
+// document.querySelector("#buttonSubmite").onclick=sayHello;
